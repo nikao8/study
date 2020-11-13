707c5327afa3009c11c67d9cd04e0a6931561d6c
Caio Pereira <pmscaio@gmail.com>
2020-11-12 22:00:45 -0300

    Create atividade_avaliativa.c
    em desenvolvimento

---
+++ atividade_avaliativa.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+// corretor de nota média
+
+int main() {
+    int a, b, c;
+    
+    printf("\n Digite as notas n1, n2 e n3: ");
+    scanf("%d %d %d", &n1, &n2, &n3);
+    
+    
+
+
+
+
+
+
+// calculador de número menor
+ 
+int main() {
+   int a, b, c;
+ 
+   printf("\nDigite os números a, b & c : ");
+   scanf("%d %d %d", &a, &b, &c);
+ 
+   if ((a < b) && (a < c))
+      printf("\na é o menor");
+ 
+   if ((b < c) && (b < a))
+      printf("\nb é o menor");
+ 
+   if ((c < a) && (c < b))
+      printf("\nc é o menor");
+ 
+   return(0);
+}
